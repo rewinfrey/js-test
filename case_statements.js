5339029f879a6c74f022be68d6c4ef1fd7c79a5f
joshvera <josh@joshvera.com>
2016-06-15 16:13:12 -0700

    add case statements

---
+++ case_statements.js
@@ -0,0 +1,23 @@
+switch (foo) {
+	case "hello":
+		break;
+}
+
+switch (foo) {
+}
+
+switch (foo) {
+	default:
+}
+
+switch (foo) {
+	case "hello":
+		1;
+		break;
+	case "world":
+		2;
+		break;
+	default:
+		3;
+		break;
+}
