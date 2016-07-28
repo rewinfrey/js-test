557019d883921670d292e6c4c88a8db0345206e6
joshvera <josh@joshvera.com>
2016-07-28 13:42:35 -0400

    add some for loops

---
+++ for_loops.js
@@ -0,0 +1,22 @@
+for (var a, b; c; d)
+  e;
+
+for (i = 0, init(); i < 10; i++)
+  log(y);
+
+for (;;) {
+  z;
+}
+
+for (var item in items)
+  item();
+
+for (item in items)
+  item();
+
+for (key in something && i = 0; i < n; i++) {
+  doSomething();
+}
+
+for (let item of items)
+  process(item);
